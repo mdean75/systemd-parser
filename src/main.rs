af70dab09cb0828b5e0ee214c1e3c2edbbb2449f
Michael DeAngelo <deangelomp@gmail.com>
2023-02-15 06:49:02 -0600

    minimal cli features working

--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,13 @@ fn main() {
 
     let args = Cli::parse();
 
-    // let args: Vec<OsString> = args_os().collect();
-    // println!("{:?}", args.get(1).unwrap());
     if args.parse {
         let unparsed_file = fs::read_to_string(args.in_file).expect("cannot read file");
 
         let parsed = systemd_parser::parser::systemd::parse(unparsed_file.as_str());
 
-        // if let Some(out_file) = pargs.out_file {
-        //     fs::write(pargs.out_file, serde_json::to_string_pretty(&parsed).unwrap()).expect("TODO: panic message");
-        // } else {
-        //     println!("{}", serde_json::to_string_pretty(&parsed).unwrap());
-        //
-        // }
         let json_data = serde_json::to_string_pretty(&parsed).unwrap();
 
-        // let mut d: SystemdFile = serde_json::from_str(json_data.as_str()).unwrap();
-        // d.service.exec_start[0] = "ExecStart=/new/path/to/binary".to_string();
-
-        // println!("deserialized: \n\n{d}");
         fs::write(args.out_file, json_data.to_string()).unwrap();
     } else {
         let json_file = fs::read_to_string(args.in_file).expect("cannot read file");
