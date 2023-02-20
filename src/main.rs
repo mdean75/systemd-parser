fad49878b54b370c2fc9f430ce475ad096ee760f
Michael DeAngelo <deangelomp@gmail.com>
2023-02-19 20:08:46 -0600

    added flag to run ip addr parser

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::process::Command;
 
 use clap::{Parser};
 use systemd_parser::parser::systemd::{SystemdFile};
@@ -7,6 +8,17 @@ use systemd_parser::parser;
 fn main() {
 
     let args = Cli::parse();
+
+    if args.ipaddr {
+
+        let cmd_out = Command::new("/bin/ip").arg("a").output().expect("command out");
+        let parse_result = parser::ipaddr::parse(String::from_utf8(cmd_out.stdout).unwrap().as_str());
+        match parse_result {
+            Ok(_) => println!("Successful parse"),
+            Err(e) => eprintln!("failed to parse: {e}")
+        }
+        return;
+    }
 
     if args.validate {
         match parser::systemd::parse(args.in_file.as_str()) {
@@ -23,7 +35,7 @@ fn main() {
     if args.parse {
         let unparsed_file = fs::read_to_string(args.in_file).expect("cannot read file");
 
-        let parsed = systemd_parser::parser::systemd::parse(unparsed_file.as_str()).unwrap();
+        let parsed = parser::systemd::parse(unparsed_file.as_str()).unwrap();
 
         let json_data = serde_json::to_string_pretty(&parsed).unwrap();
 
@@ -56,4 +68,7 @@ pub struct Cli {
 
     #[arg(short, long)]
     pub out_file: String,
+
+    #[arg(short='a', long)]
+    pub ipaddr: bool,
 }
